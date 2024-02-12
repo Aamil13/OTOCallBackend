6a1e447bdc2652ed2d3c16955363ccb817a016a7
Aamil13 <aamil.shafi13@gmail.com>
2024-02-12 15:31:53 +0530

    vercel fix try

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ import {Server} from "socket.io";
 const app = express()
 const PORT = 5000
 
-const io = new Server(8000,{
-    cors: true,
-})
+// const io = new Server(8000,{
+//     cors: true,
+// })
 
 
 app.get("/home",(req,res)=>{
@@ -17,22 +17,22 @@ app.get("/home",(req,res)=>{
 })
 
 
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id);
+// io.on("connection", (socket) => {
+// 	socket.emit("me", socket.id);
 
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	});
+// 	socket.on("disconnect", () => {
+// 		socket.broadcast.emit("callEnded")
+// 	});
 
-	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-        // console.log("aaa");
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
+// 	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+//         // console.log("aaa");
+// 		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+// 	});
 
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	});
-});
+// 	socket.on("answerCall", (data) => {
+// 		io.to(data.to).emit("callAccepted", data.signal)
+// 	});
+// });
 
 
 app.listen(PORT,()=>{
